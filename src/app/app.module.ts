177302bf2fb81ce955fcdf5ada08cd111dfd9bf7
vikas sharma <vikas.sharma59@gamil.com>
2022-11-15 23:42:11 +0530

    worked on recipes and shopping list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
 import { ServerComponent } from './server/server.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 @NgModule({
   declarations: [
     AppComponent,
     ServersComponent,
-    ServerComponent
+    ServerComponent,
+    ShoppingListComponent
    ],
   imports: [
     BrowserModule,
